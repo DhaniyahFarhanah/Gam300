2cafde041243a5e91295c60ee1b960b4e577b33c
JordanTanYanBoon <jordanyanboon.tan@digipen.edu>
2024-09-12 04:29:46 -0700

    Simple car AI

---
+++ Assets/Scripts/AI/AICarEngine.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICarEngine : MonoBehaviour
+{
+    public Transform path;
+    public float maxSteerAngle = 45f;
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+
+    private List<Transform> nodes = new List<Transform>();
+    private int currentNode = 0;
+
+    private void Start()
+    {
+        Transform[] pathTransform = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 0; i < pathTransform.Length; ++i)
+        {
+            if (pathTransform[i] != path.transform)
+            {
+                nodes.Add(pathTransform[i]);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        ApplySteer();
+        Drive();
+        CheckWaypointDistance();
+    }
+
+    private void ApplySteer()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+
+        wheelFL.steerAngle = newSteer;
+        wheelFR.steerAngle = newSteer;
+    }
+
+    private void Drive()
+    {
+        wheelFL.motorTorque = 50f;
+        wheelFR.motorTorque = 50f;
+    }
+
+    private void CheckWaypointDistance()
+    {
+        if (Vector3.Distance(transform.position, nodes[currentNode].position) < 1f)
+        {
+            if (currentNode == nodes.Count - 1)
+            {
+                currentNode = 0;
+            }
+            else
+            {
+                ++currentNode;
+            }
+            Debug.Log(currentNode);
+        }
+    }
+}
