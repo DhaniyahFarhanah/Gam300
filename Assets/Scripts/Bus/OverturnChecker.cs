e150484ac220bdaf681a440d660e7b418200f70d
DhaniyahFarhanah <noobygal21@gmail.com>
2024-09-19 19:30:13 +0800

    I FORGOT TO PUSH

--- Assets/Scripts/Bus/OverturnChecker.cs
+++ Assets/Scripts/Bus/OverturnChecker.cs
@@ -9,6 +9,7 @@ public class OverturnChecker : MonoBehaviour
     [SerializeField] private GameObject m_BusVisual;
     [SerializeField] private GameObject m_CloudSpawn;
 
+    private Vector3 m_BusRotation;
     private Vector3 m_BusTransform;
  
     // Start is called before the first frame update
@@ -21,15 +22,19 @@ public class OverturnChecker : MonoBehaviour
     void Update()
     {
         Vector3 eulers = this.transform.rotation.eulerAngles;
+        Debug.Log(m_Bus.GetComponent<Rigidbody>().velocity.magnitude);
 
         if (Input.GetKeyUp(KeyCode.R)) 
         { 
             //flip bus back
             StartCoroutine(FlipBusBack());
         }
-        
-        else if((eulers.z > 90f || eulers.z < -90f) && m_Bus.Velocity.magnitude <= 0.1f)
+                
+        else if((eulers.z >= 90f || eulers.z <= -90f || eulers.x >= 90f || eulers.x <= -90f) && m_Bus.GetComponent<Rigidbody>().velocity.magnitude <= 0.0001f)
         {
+
+            Debug.Log("Overturned");
+            m_Bus.GetComponent<Rigidbody>().velocity = Vector3.zero;
             StartCoroutine(FlipBusBack());
         }
     }
@@ -37,13 +42,14 @@ public class OverturnChecker : MonoBehaviour
     IEnumerator FlipBusBack()
     {
         yield return new WaitForSeconds(0.2f);
-        m_BusTransform = transform.rotation.eulerAngles;
         m_Bus.enabled = false;
+        m_BusRotation = transform.rotation.eulerAngles;
+        m_BusTransform = transform.position;
         m_BusVisual.SetActive(false);
-        transform.rotation = Quaternion.Euler(new Vector3(m_BusTransform.x, m_BusTransform.y, 0.0f));
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.3f);
+        transform.rotation = Quaternion.Euler(new Vector3(m_BusRotation.x, m_BusRotation.y, 0.0f));
+        transform.position = new Vector3(m_BusTransform.x, m_BusTransform.y + 0.2f, m_BusTransform.z);
         m_BusVisual.SetActive(true);
-        transform.rotation = Quaternion.Euler(new Vector3(m_BusTransform.x, m_BusTransform.y, 0.0f));
         m_Bus.enabled = true;
     }
 }
