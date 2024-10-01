9347bf4f61f0ec76ef1a10246b57bfbc38edad6f
JordanTanYanBoon <jordanyanboon.tan@digipen.edu>
2024-10-01 17:29:58 +0800

    rouge cars avoiding

--- Assets/Scripts/AI/RougeAI.cs
+++ Assets/Scripts/AI/RougeAI.cs
@@ -13,6 +13,13 @@ public class RougeAI : AICarEngine
     private GameObject player;
     private Node playerNearestNode;
     private List<Transform> waypoints = new List<Transform>();
+    public enum AIState
+    {
+        enroute,
+        ChasingPlayer,
+    }
+    public AIState State = AIState.enroute;
+    public bool avoiding = false;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +38,8 @@ public class RougeAI : AICarEngine
         EngineUpdate();
         CheckWaypointDistance();  // Check if the car is near the current waypoint
     }
+
+    #region Pathfinding
     private IEnumerator DelayedPathfinding()
     {
         // Wait for 1 second
@@ -60,6 +69,8 @@ public class RougeAI : AICarEngine
         {
             targetPosition = waypoints[0].position;
         }
+
+        State = AIState.enroute;
     }
 
     private void CheckWaypointDistance()
@@ -89,18 +100,68 @@ public class RougeAI : AICarEngine
                 if (Vector3.Distance(transform.position, player.transform.position) < Vector3.Distance(transform.position, targetPosition))
                 {
                     targetPosition = player.transform.position;
+                    State = AIState.ChasingPlayer;
                 }
             }
             else
             {
                 // No waypoints left, go directly to the player
                 targetPosition = player.transform.position;
+                State = AIState.ChasingPlayer;
             }
         }
         else
         {
             // No waypoints left, go directly to the player
             targetPosition = player.transform.position;
+            State = AIState.ChasingPlayer;
         }
     }
+    #endregion Pathfinding
+
+    #region DrivingLogic
+    protected override void ObstacleResponse()
+    {
+        if (!detectedObstacle)
+        {
+            avoiding = false;
+        }
+        else
+        {
+            ObstacleTag sensedObstacleTag = detectedObstacleHit.collider.gameObject.GetComponent<ObstacleType>().obstacleTag;
+
+            if (State == AIState.enroute && sensedObstacleTag == ObstacleTag.Light || sensedObstacleTag == ObstacleTag.Medium || sensedObstacleTag == ObstacleTag.Heavy || sensedObstacleTag == ObstacleTag.CarAI)
+            {
+                avoiding = true;
+            }
+        }
+    }
+
+    protected override void BrakeLogic()
+    {
+        isBraking = false;
+        if (slowWhenAvoiding && avoiding && currentSpeed > maxSpeed * highSpeedThreshold)
+        {
+            isBraking = true;
+        }
+        else if (slowWhenTurning && Mathf.Abs(wheelFL.steerAngle) >= maxSteerAngle * sharpTurnThreshold && currentSpeed > maxSpeed * highSpeedThreshold)
+        {
+            isBraking = true;
+        }
+    }
+
+    protected override void ApplySteer()
+    {
+        if (avoiding)
+        {
+            targetSteerAngle = maxSteerAngle * avoidMultiplier;
+        }
+        else
+        {
+            Vector3 relativeVector = transform.InverseTransformPoint(targetPosition);
+            float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+            targetSteerAngle = newSteer;
+        }
+    }
+    #endregion DrivingLogic
 }
